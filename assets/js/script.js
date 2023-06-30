643e5913898d2355036ac280a6366936e70928f7
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-06-30 12:04:40 -0400

    project finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,30 @@ var emailError = document.getElementById('required');
 var submitButton = document.getElementById('submit');
 
 function validateEmail() {
-    emailField.style.background = 'rgba(255, 0, 0, 0.2)';
+    emailField.style.background = 'rgba(255, 0, 0, 0.1)';
 
     if (!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.style.display = 'inline-block';
         emailField.style.borderColor = 'red';
-        emailError.style.display = 'inline';
         return false;
-    }
-
-    emailError.innerHTML = "";
-    emailField.style.background = 'rgba(0, 255, 0, 0.2)';
+    } else {
+        emailField.style.background = 'rgba(0, 255, 0, 0.1)';
     emailField.style.borderColor = 'green';
     emailError.style.display = 'none';
     return true;
+    }
 }
 
-function submit() {
+var btn = document.querySelector("#submit");
+
+btn.addEventListener("click", function (event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+    var text = document.querySelector("#iemail").value;
+    localStorage.setItem('valueText', text);
+    submitForm(); // Chama a função submitForm
+});
+
+function submitForm() {
     if (validateEmail()) {
         window.location.href = 'subscribing/subscribing.html';
     } else {
